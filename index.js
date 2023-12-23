443df15b3c20ef32a48d7a6f74fc227e88f14365
Muhammad Bin Usman <muhammadbin.2003@gmail.com>
2023-12-24 02:14:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const port = 3002;
+const port = process.env.PORT || 8080;
 
 // Middleware to parse JSON in requests
 app.use(express.json());
